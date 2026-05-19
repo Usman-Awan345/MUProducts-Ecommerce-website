b1d766ee4bf21d54507265fc9e961d0d3aacd2e0
Usman-Awan345 <usmanawan2124@gmail.com>
2026-05-19 21:07:35 +0500

    Fix middleware import casing

--- backend/routes/orderRoute.js
+++ backend/routes/orderRoute.js
@@ -1,7 +1,7 @@
 import express from "express";
 import { allOrders, placeOrder, placeOrderStripe, updateStatus, userOrders, verifyStripe } from "../controller/orderController.js";
-import adminAuth from "../middleware/adminAuth.js";
-import authUser from "../middleware/auth.js";
+import adminAuth from "../middleware/AdminAuth.js";
+import authUser from "../middleware/Auth.js";
 
 const orderRouter = express.Router();
 
